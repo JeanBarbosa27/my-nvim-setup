8d234ad0e074fe1d162e6168b223d2418bd5e7c8
Jean Barbosa <jean.barbosa@sky.uk>
2025-08-26 11:57:26 +0100

    config: set clipboard and wrap options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,4 +3,7 @@ local set = vim.opt
 set.number = true
 set.relativenumber = true
 set.colorcolumn = "122"
+set.clipboard = "unnamedplus"
+set.wrap = true
+
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = "gray", bg = "gray" })
