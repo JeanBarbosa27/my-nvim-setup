d5468a14ee479323a777ee2a17037d80b65bacef
Jean Barbosa <jeanbarbosa.dev@hotmail.com>
2025-09-08 17:40:22 +0100

    refact: move telescope key maps to a separate file
    Also make use of the utils functions to set the key maps, this way is
    much easier to make things using the same leader key, also make it
    easier to track all mappings.

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,69 +1,4 @@
-function set_normal_key_maps()
-  local set = vim.keymap.set
-  -- check out for ":Telescope builtin" for more builtin function references
-  local builtins = require("telescope.builtin")
-
-  -- find buffers
-  set("n", "<space>fb", builtins.buffers)
-
-  -- find files in project root
-  set("n", "<space>fD", builtins.find_files)
-
-  -- find files in current file directory
-  set("n", "<space>fd", function()
-    local current_file = vim.api.nvim_buf_get_name(0)
-    local current_dir = vim.fn.fnamemodify(current_file, ":h")
-    builtins.find_files({
-      cwd = current_dir,
-      prompt_title = "Files in current directory",
-    })
-  end)
-
-  -- find editor config
-  set("n", "<space>ec", function()
-    builtins.find_files({ cwd = vim.fn.stdpath("config") })
-  end)
-
-  -- find editor packages
-  set("n", "<space>ep", function()
-    builtins.find_files {
-      cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-    }
-  end)
-
-  -- ssh config
-  local ssh_folder = "~/.ssh"
-
-  set("n", "<space>es", function()
-    vim.cmd.edit(vim.fs.joinpath(ssh_folder, "/config"))
-  end, { desc = "Edit ssh config" })
-
-  set("n", "<space>sc", function()
-    builtins.find_files { cwd = ssh_folder }
-  end, { desc = "Find in SSH config" })
-
-  -- edit zsh config
-  set("n", "<space>ez", function()
-    vim.cmd.edit("~/.zshrc")
-  end, { desc = "Edit zsh config" })
-
-  -- find zsh config
-  set("n", "<space>zc", function()
-    builtins.find_files { cwd = "~/.config/zsh", hidden = true }
-  end)
-
-  -- find help tags
-  set("n", "<space>fh", builtins.help_tags)
-
-  -- find references
-  set("n", "<space>fr", builtins.lsp_references)
-
-  -- find text in the current buffer
-  set("n", "<space>ft", builtins.current_buffer_fuzzy_find)
-
-  -- find text in the current project
-  set("n", "<space>F", builtins.live_grep)
-end
+local telescope_key_maps = require("config.telescope.mappings")
 
 return {
   {
@@ -99,7 +34,7 @@ return {
       require("telescope").load_extension("fzf")
       require("config.telescope.multigrep").setup()
 
-      set_normal_key_maps()
+      telescope_key_maps.set_normal_key_maps()
     end
   }
 }
---
+++ lua/config/telescope/mappings.lua
@@ -0,0 +1,77 @@
+local utils = require("config.utils")
+
+local M = {}
+
+function M.find_in_buffer(builtins)
+  -- find buffers
+  utils.set_nkey("fb", builtins.buffers)
+
+  -- find symbols
+  utils.set_nkey("fs", builtins.lsp_document_symbols, { desc = "Find symbols in current buffer" })
+
+  -- find references
+  utils.set_nkey("fr", builtins.lsp_references)
+
+  -- find text
+  utils.set_nkey("ft", builtins.current_buffer_fuzzy_find)
+end
+
+function M.find_in_workspace(builtins)
+  -- find files in project root
+  utils.set_nkey("fD", builtins.find_files)
+
+  -- find files in current file directory
+  utils.set_nkey("fd", function()
+    local current_file = vim.api.nvim_buf_get_name(0)
+    local current_dir = vim.fn.fnamemodify(current_file, ":h")
+    builtins.find_files({ cwd = current_dir, prompt_title = "Files in current directory", })
+  end)
+
+  -- find text
+  utils.set_nkey("F", builtins.live_grep)
+
+  -- find symbols
+  utils.set_nkey("fw", builtins.lsp_workspace_symbols, { desc = "Find symbos in current workspace" })
+end
+
+function M.find_config_files(builtins)
+  -- find editor config
+  utils.set_nkey("ec", function() builtins.find_files({ cwd = vim.fn.stdpath("config") }) end)
+
+  -- find editor packages
+  utils.set_nkey("ep", function()
+    builtins.find_files { cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy") }
+  end)
+
+  -- ssh config
+  local ssh_folder = "~/.ssh"
+  utils.set_nkey("es", function()
+    vim.cmd.edit(vim.fs.joinpath(ssh_folder, "/config"))
+  end, { desc = "Edit ssh config" })
+
+  utils.set_nkey("sc", function()
+    builtins.find_files { cwd = ssh_folder }
+  end, { desc = "Find in SSH config" })
+
+  -- edit zsh config
+  utils.set_nkey("ez", function() vim.cmd.edit("~/.zshrc") end, { desc = "Edit zsh config" })
+
+  -- find zsh config
+  utils.set_nkey("zc", function()
+    builtins.find_files { cwd = "~/.config/zsh", hidden = true }
+  end)
+
+  -- find Telescope help tags
+  utils.set_nkey("fh", builtins.help_tags)
+end
+
+function M.set_normal_key_maps()
+  -- check out for ":Telescope builtin" for more builtin function references
+  local builtins = require("telescope.builtin")
+
+  M.find_in_buffer(builtins)
+  M.find_in_workspace(builtins)
+  M.find_config_files(builtins)
+end
+
+return M
