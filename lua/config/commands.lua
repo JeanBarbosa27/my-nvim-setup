fb4d5a2ed67e629656f893be436355afdbb2b3a8
Jean Barbosa <jeanbarbosa.dev@hotmail.com>
2025-09-16 19:25:22 +0100

    feat: create commands to copy from file system

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -1,5 +1,24 @@
--- Close all other buffers
-vim.api.nvim_create_user_command("BOnly", function()
+local utils = require("config.utils")
+
+local set_command = vim.api.nvim_create_user_command
+
+set_command("BOnly", function()
   local current_buffer_number = vim.fn.bufnr("%")
   vim.cmd("bufdo exe 'if bufnr() != " .. current_buffer_number .. " | bdelete | endif'")
 end, { desc = "Close all buffers other than current" })
+
+set_command("CopyFileName", function()
+  utils.copy_to_clipboard(utils.get_file_name())
+end, { desc = "Copy file name to the clipboard" })
+
+set_command("CopyFilePath", function()
+  utils.copy_to_clipboard(utils.get_file_path())
+end, { desc = "Copy full file path to the clipboard" })
+
+set_command("CopyDirectoryPath", function()
+  utils.copy_to_clipboard(utils.get_current_directory_path())
+end, { desc = "Copy current file directory path" })
+
+set_command("CopyProjectRootPath", function()
+  utils.copy_to_clipboard(utils.get_project_root_path())
+end, { desc = "Copy project root path, basically from where editor was opened" })
--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -13,4 +13,27 @@ function M.set_vkey(key, callback, opt)
   M.set_key("v", key, callback, opt)
 end
 
+function M.copy_to_clipboard(object_to_copy)
+  vim.fn.setreg("+", object_to_copy)
+  print(string.format("Copied %s to the clipboard", object_to_copy))
+end
+
+function M.get_file_name()
+  return vim.fn.expand("%:t")
+end
+
+function M.get_file_path()
+  return vim.api.nvim_buf_get_name(0)
+end
+
+function M.get_current_directory_path()
+  local file_name = M.get_file_name()
+  local full_path = M.get_file_path()
+  return full_path:gsub(file_name, "")
+end
+
+function M.get_project_root_path()
+  return vim.fn.system("pwd"):gsub("\n", "")
+end
+
 return M
