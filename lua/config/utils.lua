67a7c10e7ba94ccfe10a4a0762e0edaedace8889
Jean Barbosa <jeanbarbosa.dev@hotmail.com>
2025-09-22 07:40:29 +0100

    refact: make utils.set_key more flexible

--- lua/config/utils.lua
+++ lua/config/utils.lua
@@ -2,15 +2,19 @@ local M = {}
 
 function M.set_key(mode, key, callback, opts)
   local keymap_opts = opts or {}
-  vim.keymap.set(mode, "<space>" .. key, callback, keymap_opts)
+  vim.keymap.set(mode, key, callback, keymap_opts)
+end
+
+function M.set_key_with_leader(mode, key, callback, opt)
+  M.set_key(mode, "<space>" .. key, callback, opt)
 end
 
 function M.set_nkey(key, callback, opt)
-  M.set_key("n", key, callback, opt)
+  M.set_key_with_leader("n", key, callback, opt)
 end
 
 function M.set_vkey(key, callback, opt)
-  M.set_key("v", key, callback, opt)
+  M.set_key_with_leader("v", key, callback, opt)
 end
 
 function M.copy_to_clipboard(object_to_copy)
