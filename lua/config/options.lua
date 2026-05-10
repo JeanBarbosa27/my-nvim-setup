faae70ee1fe24d0e71d21d06293df86cc1445e8d
Jean Barbosa <jbarbosa.wk@gmail.com>
2026-05-10 22:25:58 +0100

    feat: configure SpellBad highlights

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,12 @@ set.clipboard = "unnamedplus"
 set.colorcolumn = "122"
 set.number = true
 set.relativenumber = true
+set.spell = true
+set.spelllang = { "en_gb" }
 set.updatetime = 100
 set.wrap = true
 
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = "gray", bg = "gray" })
+vim.cmd([[
+  highlight SpellBad cterm=undercurl gui=undercurl guisp=red
+]])
