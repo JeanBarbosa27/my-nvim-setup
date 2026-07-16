b748f72f27831a28f7eb77d3704c99cafa4c50bf
Jean Barbosa <jeanbarbosa.dev@hotmail.com>
2026-07-16 18:00:20 +0100

    feat: add stop keymap and standard db key
    `db` key stands for debug.

--- lua/config/plugins/nvim-dap.lua
+++ lua/config/plugins/nvim-dap.lua
@@ -20,9 +20,10 @@ return {
       { "<F10>",      function() require("dap").step_over() end,         desc = "Debug: Step Over" },
       { "<F11>",      function() require("dap").step_into() end,         desc = "Debug: Step Into" },
       { "<F12>",      function() require("dap").step_out() end,          desc = "Debug: Step Out" },
+      { "<S-F5>",     function() require("dap").terminate() end,         desc = "Debug: Stop" },
       { "<space>db",  function() require("dap").toggle_breakpoint() end, desc = "Debug: Toggle Breakpoint" },
-      { "<space>ddu", function() require("dapui").toggle() end,          desc = "Debug: Toggle UI" },
-      { "<space>ddt", function() require("dap-go").debug_test() end,     desc = "Debug: Nearest Go test" },
+      { "<space>dbu", function() require("dapui").toggle() end,          desc = "Debug: Toggle UI" },
+      { "<space>dbt", function() require("dap-go").debug_test() end,     desc = "Debug: Nearest Go test" },
     },
     config = function()
       local dap, dapui = require("dap"), require("dapui")
